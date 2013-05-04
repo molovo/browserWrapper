8bf3e9718d059f4532ea3a3a16b64d46a327d599
molovo <hi@molovo.co.uk>
2013-05-04 12:30:20 +0100

    Browser controls working

--- src/js/jquery.browserwrapper.js
+++ src/js/jquery.browserwrapper.js
@@ -44,6 +44,7 @@
 
         if (defaults.makeBrowserWork && (currentContent.match(/<iframe/) || currentContent.match(/<object/))) {
             var contentEditable = 'contenteditable="true"';
+            act.data('originalURL', act.find('iframe').attr('src') || act.find('object').attr('data'));
         }
 
         if( defaults.os == 'detect' || defaults.browser == 'detect' ){
@@ -73,11 +74,12 @@
                         subString: "Chrome",
                         identity: "chrome"
                     },
-                    {
+                    //Commented out until added
+                    /*{
                         string: navigator.vendor,
                         subString: "Apple",
                         identity: "safari"
-                    },
+                    },*/
                     {
                         string: navigator.userAgent,
                         subString: "Firefox",
@@ -94,12 +96,14 @@
                         string: navigator.platform,
                         subString: "Mac",
                         identity: "mac"
-                    },
+                    }
+                    //Commented out until added
+                    /*,
                     {
                         string: navigator.platform,
                         subString: "Linux",
                         identity: "linux"
-                    }
+                    }*/
                 ]
 
             };
@@ -114,20 +118,33 @@
         } else if ( defaults.os == 'windows' ) {
             var windowButtons = '<img class="window-button" src="' + defaults.filePath + 'img/windows-buttons.png" />';
         }
+        if(defaults.browser) {
         var browserTab = '<span class="tab ' + defaults.browser + '"><span class="tab-wrapper"><img class="favicon" src="' + defaults.favicon + '" /><span class="tabtext">' + defaults.browserTitle + '</span></span></span>'
-        var browserControls = '<div class="browser-gui-controls ' + defaults.browser + '"><img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-nav-buttons.png" /><span class="address-bar notsearch">';
-                if (defaults.browser == 'chrome') {
-                    browserControls += '<img class="address-bar-icon" src="' + defaults.filePath + 'img/address-bar-icon.png" />';
-                }
-                browserControls += '<span class="address-bar-text" ' + contentEditable + '>' + defaults.browserURL + '</span>';
-                if (defaults.browser == 'chrome') {
-                    browserControls += '<img class="address-bar-bookmark" src="' + defaults.filePath + 'img/' + defaults.browser + '-address-bar-bookmark.png" /></span><img class="nav-button-settings" src="' + defaults.filePath + 'img/nav-button-settings.png" />';
-                } else if (defaults.browser == 'firefox') {
-                    browserControls += '<img class="address-bar-bookmark" src="' + defaults.filePath + 'img/' + defaults.browser + '-address-bar-bookmark.png" /></span><span class="address-bar firefox-google-search"><img class="address-bar-icon" src="' + defaults.filePath + 'img/google-search-icon.png" /><span class="address-bar-text">Search Google</span></span>'
+        var browserControls = '<div class="browser-gui-controls ' + defaults.browser + '">';
+            if (defaults.browser == 'chrome') {
+                if (contentEditable) {
+                    browserControls += '<a class="back-button"><img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-back-button.png" /></a><a class="forward-button"><img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-forward-button.png" /></a><a href="#" class="refresh-button"><img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-refresh-button.png" /></a><a href="#" class="home-button"><img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-home-button.png" /></a>';
                 } else {
-                    browserControls += '</span>';
+                    browserControls += '<img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-nav-buttons.png" />';
                 }
-                browserControls += '</div>';
+                browserControls += '<span class="address-bar notsearch"><img class="address-bar-icon" src="' + defaults.filePath + 'img/address-bar-icon.png" />';
+            } else if (defaults.browser == 'firefox') {
+                    browserControls += '<img class="nav-buttons" src="' + defaults.filePath + 'img/' + defaults.browser + '-nav-buttons.png" />';
+                browserControls += '<span class="address-bar notsearch"><img class="address-bar-icon" src="' + defaults.filePath + 'img/address-bar-icon.png" />';
+            }
+            browserControls += '<span class="address-bar-text" ' + contentEditable + '>' + defaults.browserURL + '</span>';
+            if (defaults.browser == 'chrome') {
+                browserControls += '<img class="address-bar-bookmark" src="' + defaults.filePath + 'img/' + defaults.browser + '-address-bar-bookmark.png" /></span><img class="nav-button-settings" src="' + defaults.filePath + 'img/nav-button-settings.png" />';
+            } else if (defaults.browser == 'firefox') {
+                browserControls += '<img class="address-bar-bookmark" src="' + defaults.filePath + 'img/' + defaults.browser + '-address-bar-bookmark.png" /></span><span class="address-bar firefox-google-search"><img class="address-bar-icon" src="' + defaults.filePath + 'img/google-search-icon.png" /><span class="address-bar-text">Search Google</span></span>'
+            } else {
+                browserControls += '</span>';
+            }
+            browserControls += '</div>';
+        } else {
+            browserControls = ' ';
+            browserTab = ' ';
+        }
 
         var windowControls  = '<div class="browser-window-controls ' + defaults.os + ' ' + defaults.browser + '">' + windowButtons + browserTab + '</div>';
 
@@ -135,6 +152,11 @@
         act.html('<div class="browser-window-border ' + defaults.os + ' ' + defaults.browser + '" style="box-shadow:' + defaults.shadow + '">' + windowControls + '<div class="browser-gui ' + defaults.browser + '">' + browserControls + currentContent + '</div></div>').trigger('refresh');
 
         if (contentEditable) {
+
+            // Update home and refresh links with origin URL
+            act.find('.home-button').attr('href', act.data('originalURL'));
+            act.find('.refresh-button').attr('href', act.data('originalURL'));
+
             act.on('focus', '.address-bar-text[contenteditable]', function() {
                 // Take current value from address bar
                 var $this = $(this);
@@ -169,23 +191,65 @@
                     $this.html(newURL);
                 }
 
-                // update iframe src directly
-                act.find('iframe').attr('src', newURL);
+                rebuildBrowser.init(act, newURL);
 
-                // for objects, object must be cloned, removed, and have the data source modified before re-adding to the DOM
-                var newObj = act.find('object').clone().attr('data', newURL);
-                act.find('object').remove();
-                act.find('.browser-content').append( newObj );
-
-                // Take a lucky guess as to the favicon location. If wrong, load the default
-                var newFavicon = newURL.split('/').splice(0,3).join('/')+'/favicon' + ('.ico' || '.png');
-                act.find('.favicon').attr('onerror', 'this.src=\'' + defaults.favicon + '\'').attr('src', newFavicon);
-
-                // Grab the title of the new page (Only works on same domain)
-                var newTitle = act.find('object' || 'iframe').document.title;
-                act.find('.tabtext').html( newTitle );
+                // Make full screen button open iframe URL in a new tab
+                act.find('.window-button.green').wrap('<a href="' + newURL + '" target="_blank" />');
+                // Make back button work properly
+                act.find('.back-button').attr('href', $this.data('before')).on('click', function(event) {
+                    event.preventDefault();
+                    var forwardURL = act.find('iframe').attr('src') || act.find('object').attr('data');
+                    act.find('.forward-button').attr('href', forwardURL);
+                    rebuildBrowser.init(act, $(this).attr('href'));
+                    return false;
+                });
+                // Make forward button work properly
+                act.find('.forward-button').on('click', function(event) {
+                    event.preventDefault();
+                    var backURL = act.find('iframe').attr('src') || act.find('object').attr('data');
+                    act.find('.back-button').attr('href', backURL);
+                    rebuildBrowser.init(act, $(this).attr('href'));
+                    return false;
+                });
+                // Make refresh button work properly
+                act.find('.refresh-button').on('click'), function(event) {
+                    event.preventDefault();
+                    var refreshURL = act.find('iframe').attr('src') || act.find('object').attr('data');
+                    rebuildBrowser.init(act, refreshURL);
+                    return false;
+                }
+                // Make home button work properly
+                act.find('.home-button').on('click'), function(event) {
+                    event.preventDefault();
+                    var homeURL = act.data('originalURL');
+                    rebuildBrowser.init(act, homeURL);
+                    return false;
+                }
             });
-        }
 
+            var rebuildBrowser = {
+                init: function(target, url) {
+                    // update iframe src directly
+                    target.find('iframe').attr('src', url);
+
+                    // for objects, object must be cloned, removed, and have the data source modified before re-adding to the DOM
+                    var newObj = target.find('object').clone().attr('data', url);
+                    target.find('object').remove();
+                    target.find('.browser-content').append( newObj );
+
+                    // Take a lucky guess as to the favicon location
+                    var newFavicon = url.split('/').splice(0,3).join('/') + '/favicon' + ('.ico' || '.png');
+                    target.find('.favicon').attr('src', newFavicon);
+
+                    // Grabbing the title of the new page is not possible, so we'll update it with the URL
+                    target.find('.tabtext').html( url );
+                    // var newTitle = target.find('object' || 'iframe').document.title;
+                    //target.find('.tabtext').html( newTitle );
+
+                }
+            }
+
+        }
     }
+
 })(jQuery);
